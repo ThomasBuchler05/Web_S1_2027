35c4da2fd28fc4f5a0c77fcc85798bbb85f7a185
ThomasBuchler05 <thomas.buchler@edu.ece.fr>
2026-09-18 10:13:34 +0200

    pour_vincent

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ console.log("Hello Node.js!")
 // Import a module
 const http = require('http')
 
-// Declare an http server
-http.createServer(function (req, res) {
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
 
-  // Write a response header
-  res.writeHead(200, {'Content-Type': 'text/plain'})
+const url = require('url')
 
-  // Write a response content
-  res.end('Hello World\n')
+const serverHandle = function (req, res) {
+  // Retrieve and print the current path
+  const path = url.parse(req.url).pathname
+  console.log(path)
 
-// Start the server
-}).listen(8080)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write(path)
+  res.end()
+}
+
+http
+.createServer(serverHandle)
+.listen(8080)
